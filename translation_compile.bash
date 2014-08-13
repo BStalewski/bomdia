3c843f5f51119d5b4dd1b113c2ee3ca188358c73
Bartosz Stalewski <bartekstalewski@gmail.com>
2014-08-13 08:17:10 +0200

    Modify translation scripts, move translation files

--- translation_compile.bash
+++ translation_compile.bash
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-msgfmt po/pl.po --output-file po/pl.mo
+I18N_DIR="i18n"
+PROJECT_NAME="Bomdia"
+
+if [ $# != "1" ]; then
+    echo "Wrong usage. Use: $0 <lang>"
+    exit 1
+fi
+
+LANG=$1
+MESSAGES_DIR="$I18N_DIR/$LANG/LC_MESSAGES"
+
+msgfmt "$MESSAGES_DIR/$PROJECT_NAME.po" --output-file "$MESSAGES_DIR/$PROJECT_NAME.mo"
--- translation_make.bash
+++ translation_make.bash
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-xgettext --language=Python --keyword=_ --output=po/bomdia.pot `find . -name "*.py"`
+if [ $# != "1" ]; then
+    echo "Wrong usage. Use: $0 <lang>"
+    exit 1
+fi
+
+LANG=$1
+
+I18N_DIR="i18n"
+PROJECT_NAME="Bomdia"
+TEMPLATE_FILE="$I18N_DIR/$PROJECT_NAME.pot"
+PO_FILE="$I18N_DIR/$LANG/LC_MESSAGES/$PROJECT_NAME.po"
+
+# Create translation template
+xgettext --language=Python --keyword=_ "--output=$TEMPLATE_FILE" `find . -name "*.py"`
+sed -i 's/CHARSET/utf-8/' $TEMPLATE_FILE
+
+# Update translations
+if [ -f $PO_FILE ]; then
+    msgmerge -q $PO_FILE $TEMPLATE_FILE --output-file=$PO_FILE
+else
+    msginit "--input=$TEMPLATE_FILE" "--output-file=$PO_FILE" "--locale=$LANG"
+fi
